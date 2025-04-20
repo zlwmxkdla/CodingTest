4eeb4954d560e49ac5ad9f5b4bd7c10e1fb730d3
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-04-20 22:09:25 +0900

    [Silver III] Title: 도시와 비트코인, Time: 208 ms, Memory: 22988 KB -BaekjoonHub

--- 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.java
+++ 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.java
@@ -1,44 +1,54 @@
 
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	public static void main(String[]args) throws IOException{
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][]arr = new int[M][N];
-		boolean[][]visited = new boolean[M][N];
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(bf.readLine());
-			for(int j=0;j<N;j++) {
-				arr[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		int []dx= {0,1};
-		int []dy= {1,0};
-		
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[]{0,0});
-		visited[0][0]=true;
-		while(!queue.isEmpty()) {
-			int[]now=queue.poll();
-			if(now[0]==M-1&&now[1]==N-1) {
-				System.out.println("Yes");
-				return;
-			}
-			for(int i=0;i<2;i++) {
-				int new_x=now[0]+dx[i];
-				int new_y=now[1]+dy[i];
-				
-				if((new_x>=0&&new_x<M)&&(new_y>=0&&new_y<N)&&arr[new_x][new_y]==1&&!visited[new_x][new_y]) {
-					queue.add(new int[] {new_x,new_y});
-					visited[new_x][new_y]=true;
-				}
-			}
-		}
-		System.out.println("No");
-	}
+    static int N, M;
+    static int[][] arr;
+    static boolean[][] visited;
+    static boolean arrived = false;
+    static int[] dx = {0, 1}, dy = {1, 0}; // 오른쪽, 아래
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 가로
+        M = Integer.parseInt(st.nextToken()); // 세로
+        arr = new int[M][N];
+        visited = new boolean[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        if (arr[0][0] == 1) {
+            dfs(0, 0);
+        }
+
+        System.out.println(arrived ? "Yes" : "No");
+    }
+
+    public static void dfs(int x, int y) {
+        if (x == M - 1 && y == N - 1) {
+            arrived = true;
+            return;
+        }
+
+        visited[x][y] = true;
+
+        for (int i = 0; i < 2; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+                if (!visited[nx][ny] && arr[nx][ny] == 1) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
 }
