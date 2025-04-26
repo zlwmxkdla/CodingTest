26386f2090b03c764dc3abb1a3227a213baee9e3
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-04-26 16:59:51 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Main {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[][]dp = new int[N+1][N+1];
+		
+		for(int i=0;i<=N;i++) {
+			dp[i][1]=i;
+			dp[i][0]=1;
+			dp[i][i]=1;
+		}
+		
+		for(int i=2;i<=N;i++) {
+			for(int j=1;j<i;j++) {
+				dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+			}
+		}
+		System.out.println(dp[N][K]);
+		sc.close();
+	}
+}
