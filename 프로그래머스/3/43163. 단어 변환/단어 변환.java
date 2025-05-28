1d5b0f58309949b88f19a63232b202f892b7d7bf
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-05-29 00:41:32 +0900

    [level 3] Title: 단어 변환, Time: 0.65 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,40 @@
 import java.util.*;
-
 class Solution {
-    static boolean[]visited;
-    static int answer = Integer.MAX_VALUE;
+    class wordstats{
+        int cnt;
+        String word;
+        wordstats(int cnt,String word){
+            this.cnt=cnt;
+            this.word=word;
+        }
+    }
     public int solution(String begin, String target, String[] words) {
-        Set<String> set = new HashSet<>(Arrays.asList(words));
-        if(!set.contains(target))return 0;
+        Queue<wordstats> q= new LinkedList<>();
+        q.add(new wordstats(0,begin));
+        boolean[]visited = new boolean[words.length];
         
-        
-        for(int i=0;i<words.length;i++){
+        while(!q.isEmpty()){
+            wordstats now = q.poll();
+            if(now.word.equals(target))return now.cnt;
             
-            if(can_change(begin, words[i])){
-               visited = new boolean[words.length];
-                visited[i]=true; 
-                dfs(words[i],target,words, 1);
-            }
-            
-        }
-        
-        return answer;
-    }
-    
-    public void dfs(String begin, String target, String[]words, int depth){
-        if(begin.equals(target)){
-            answer = Math.min(answer,depth);
-            return;
-        }
-        for(int i=0;i<words.length;i++){
-            if(!visited[i]&&can_change(begin,words[i])){
-                visited[i]=true;
-                dfs(words[i],target,words,depth+1);
-                visited[i]=false;
+            for(int i=0;i<words.length;i++){
+                if(!visited[i]){
+                    
+                    if(canchange(now.word,words[i])){
+                        visited[i]=true;
+                        q.add(new wordstats(now.cnt+1,words[i]));
+                    }
+                }
             }
         }
+        return 0;
     }
-    
-    
-    
-    public boolean can_change(String a, String b){
-        int count = 0;//다른 알파벳의 개수
+    public boolean canchange(String a, String b){
+        int count = 0;
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!=b.charAt(i))count+=1;
-            if(count>1)return false;
         }
-        if(count==0)return false;
-        return true;
+        if(count==1)return true;
+        else return false;
     }
 }
