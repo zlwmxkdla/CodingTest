a588c58d873be2042bcf33df36065199c1cd60d3
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-05-29 10:38:48 +0900

    [level 3] Title: 단어 변환, Time: 0.94 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,35 +1,37 @@
 import java.util.*;
 class Solution {
     class wordstats{
-        int cnt;
-        String word;
-        wordstats(int cnt,String word){
-            this.cnt=cnt;
-            this.word=word;
+        int num;
+        String s;
+        wordstats(int num,String s){
+            this.num=num;
+            this.s=s;
         }
     }
     public int solution(String begin, String target, String[] words) {
-        Queue<wordstats> q= new LinkedList<>();
-        q.add(new wordstats(0,begin));
+        int answer = 0;
+        
         boolean[]visited = new boolean[words.length];
+        Queue<wordstats> q = new LinkedList<>();
+        q.add(new wordstats(0,begin));
+        //visited[0]=true;//begin 단어는 시작점에 없음
         
         while(!q.isEmpty()){
             wordstats now = q.poll();
-            if(now.word.equals(target))return now.cnt;
+            if(now.s.equals(target))return now.num;
             
             for(int i=0;i<words.length;i++){
-                if(!visited[i]){
+                if(!visited[i]&&canchange(now.s,words[i])){
+                    visited[i]=true;
+                    q.add(new wordstats(now.num+1,words[i]));
                     
-                    if(canchange(now.word,words[i])){
-                        visited[i]=true;
-                        q.add(new wordstats(now.cnt+1,words[i]));
-                    }
                 }
             }
+            
         }
-        return 0;
+        return answer;
     }
-    public boolean canchange(String a, String b){
+    public static boolean canchange(String a, String b){
         int count = 0;
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!=b.charAt(i))count+=1;
