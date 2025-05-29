5aa4e320a15520860d4121fb6eba48885f148b5c
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-05-29 17:25:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.87 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,47 @@
 import java.util.*;
-
 class Solution {
-    static List<List<Integer>> list = new ArrayList<>();
-    static boolean[] visited;
-    static int sub1;
+    static boolean[]visited;
 
+    static List<List<Integer>> list;
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-
-        // 리스트 초기화
-        for (int i = 0; i < n; i++) {
-            list.add(new ArrayList<>());
-        }
-
-        // 그래프 구성
-        for (int[] wire : wires) {
-            int from = wire[0] - 1;
-            int to = wire[1] - 1;
+        list = new LinkedList<>();
+        for(int i=0;i<=n;i++)list.add(new LinkedList<>());
+        for(int i=0;i<wires.length;i++){
+            int from = wires[i][0];
+            int to = wires[i][1];
             list.get(from).add(to);
             list.get(to).add(from);
         }
-
-        // 전선 하나씩 끊어보며 최소 차이 탐색
-        for (int[] wire : wires) {
-            int from = wire[0] - 1;
-            int to = wire[1] - 1;
-
-            // 전선 끊기
-            list.get(from).remove(Integer.valueOf(to));
-            list.get(to).remove(Integer.valueOf(from));
-
-            // 차이 계산
-            visited = new boolean[n];
-            sub1 = 1;
-            visited[from] = true;
-            dfs(from);
-            int sub2 = n - sub1;
-            answer = Math.min(answer, Math.abs(sub1 - sub2));
-
-            // 다시 연결
-            list.get(from).add(to);
-            list.get(to).add(from);
+        
+        for(int i=0;i<wires.length;i++){
+            int rx = wires[i][0];
+            int ry = wires[i][1];
+            list.get(rx).remove(Integer.valueOf(ry));
+            list.get(ry).remove(Integer.valueOf(rx));//연결 관계 끊기
+            visited = new boolean[n+1];
+            for(int j=1;j<=n;j++){
+                if(!visited[j]){
+                    visited[j]=true;
+                    dfs(j,0);
+                    break;
+                }
+            }
+            int depth = 0;
+            for(int j=1;j<=n;j++)if(!visited[j])depth+=1;
+            int rest = n-depth;
+            answer = Math.min(answer,Math.abs(depth-rest));
+        
+            list.get(rx).add(ry);//다시 끊었던 관계 연결하기
+            list.get(ry).add(rx);
         }
-
         return answer;
     }
-
-    public void dfs(int node) {
-        for (int next : list.get(node)) {
-            if (!visited[next]) {
-                visited[next] = true;
-                sub1++;
-                dfs(next);
+    public static void dfs(int now, int depth){
+        for(Integer e:list.get(now)){
+            if(!visited[e]){
+                visited[e]=true;
+                dfs(e,depth+1);
             }
         }
     }
