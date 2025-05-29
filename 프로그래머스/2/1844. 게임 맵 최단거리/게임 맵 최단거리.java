e47a64e2f5fe6bfa9e5ce3991b06261d213603ec
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-05-29 22:13:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.95 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,24 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        int answer = -1;
-        int[]dx = {-1,0,0,1};
-        int[]dy = {0,-1,1,0};
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][]visited = new boolean[n][m];
-        q.add(new int[]{0,0,1});
-        visited[0][0]=true;
+        int[]dx={-1,0,0,1};int[]dy={0,1,-1,0};
+        int m = maps.length;
+        int n = maps[0].length;
         
+        boolean[][]visited = new boolean[m][n];
+        Queue<int[]>q=new LinkedList<>();
+        
+        q.add(new int[]{0,0,1});
         while(!q.isEmpty()){
-            int[] now = q.poll();
-            if(now[0]==n-1&&now[1]==m-1)return now[2];
-            
+            int[]now=q.poll();
+            if(now[0]==m-1&&now[1]==n-1)return now[2];
             for(int i=0;i<4;i++){
                 int nx = now[0]+dx[i];
                 int ny = now[1]+dy[i];
-                if((nx>=0&&nx<n)&&(ny>=0&&ny<m)){
+                if(nx>=0&&nx<m&&ny>=0&&ny<n){
                     if(!visited[nx][ny]&&maps[nx][ny]==1){
                         visited[nx][ny]=true;
                         q.add(new int[]{nx,ny,now[2]+1});
@@ -26,7 +23,6 @@ class Solution {
                 }
             }
         }
-        
-        return answer;
+        return -1;
     }
 }
