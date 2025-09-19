0e2aeb0a1c509cca138453db17d66689ffa52686
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-09-19 22:51:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 40.27 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,40 +1,46 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        long sum1 = 0, sum2 = 0, total = 0;
-        Queue<Integer> q1 = new ArrayDeque<>();
-        Queue<Integer> q2 = new ArrayDeque<>();
-
-        for (int n : queue1) { q1.offer(n); sum1 += n; }
-        for (int n : queue2) { q2.offer(n); sum2 += n; }
-        total = sum1 + sum2;
-
-        // 합이 홀수면 절대 같게 못 만듦
-        if ((total & 1) == 1) return -1;
-        long target = total / 2;
-
-        int limit = (queue1.length + queue2.length) * 2; // 무한 루프 방지 상한
-        int cnt = 0;
-
-        while (sum1 != target) {
-            if (cnt > limit) return -1;
-
-            if (sum1 > target) {
-                Integer val = q1.poll();
-                if (val == null) return -1; // 방어 코드
-                sum1 -= val;
-                sum2 += val;
-                q2.offer(val);
-            } else {
-                Integer val = q2.poll();
-                if (val == null) return -1;
-                sum2 -= val;
-                sum1 += val;
-                q1.offer(val);
-            }
-            cnt++;
+        int answer = 0;
+        long q1sum=0,q2sum=0;
+        for(int i=0;i<queue1.length;i++){
+            q1sum+=queue1[i];
+            q2sum+=queue2[i];
         }
-        return cnt;
+        if((q1sum+q2sum)%2==1)return -1;
+        long goal = (q1sum+q2sum)/2;
+        int n = queue1.length;
+        int m = queue2.length;
+        
+        int []total = new int[2*(n+m)];
+        
+        for(int i=0;i<n;i++)total[i]=queue1[i];
+        int index = 0;
+        for(int i=n;i<n+m;i++)total[i]=queue2[index++];
+        for(int i=n+m;i<total.length;i++)total[i]=total[i-(n+m)];
+        
+        
+        
+        int start = 0;
+        int end = n-1;
+        while(true){
+            if(start==2*(n+m)-1||end==2*(n+m)-1){
+                answer=-1;
+                break;
+            }
+            if(q1sum<goal){
+                end=(end+1)%total.length;
+                answer+=1;
+                q2sum-=total[end];
+                q1sum+=total[end];
+            }else if(q1sum>goal){
+                answer+=1;
+                q1sum-=total[start];
+                q2sum+=total[start];
+                start+=1;
+            }else break;
+            
+        }
+        
+        return answer;
     }
 }
