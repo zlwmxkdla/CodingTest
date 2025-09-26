6cd3b25503271b4186a0c4b706af985f17e77574
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-09-26 17:02:04 +0900

    [level 3] Title: 양과 늑대, Time: 8.04 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,35 +1,49 @@
 import java.util.*;
 class Solution {
+    int n;
     public int solution(int[] info, int[][] edges) {
-
-        boolean[]visited = new boolean[info.length];//방문 배열 저장하기
-        visited[0]=true;//루트 노드는 방문 처리하기
-        
-        return dfs(info,edges,visited,1,0);
+        int answer = 1;
+        // 간접 리스트 -> 인접리스트
+        n = info.length;
+        List<Integer>[] tree = new ArrayList[n];
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < info.length; i++){
+            tree[i] = new ArrayList<>();
+        }
+        for(int[] edge: edges){
+            int p = edge[0];
+            int c = edge[1];
+            tree[p].add(c);
+        }
+        visited[0] = true;
+        answer = dfs(info, tree, visited, 1, 0);
+        visited[0] = false;
+        return answer;
     }
-    public int dfs(int[]info,int[][]edges,boolean[]visited,int sheep,int wolf){
-        //늑대가 양보다 많으면
-        if(wolf>=sheep)return sheep;
-        int maxsheep = sheep;
-        //모든 에지 확인하기
-        for(int[] edge:edges){
-            int parent = edge[0];
-            int child = edge[1];
-            //부모가 방문된 상태이고 자식이 아직 방문되지 않은 상태인 경우 방문하기
-            if(visited[parent]&&!visited[child]){
-                //다음 노드 방문 처리
-                visited[child]=true;
-                //다음 노드가 양이라면
-                if(info[child]==0){
-                    maxsheep = Math.max(maxsheep,dfs(info,edges,visited,sheep+1,wolf));
-                }else{
-                    //다음 노드가 늑대라면
-                    maxsheep = Math.max(maxsheep,dfs(info,edges,visited,sheep,wolf+1));
+    // 현재 상황에서 모을 수 있는 양은 최대 몇마리?
+    int dfs(int[] info, List<Integer>[] tree, boolean[] visited, int sheep, int wolf){
+        if(sheep == wolf) return sheep;
+        int maxSheep = sheep;
+        int ret = 0;
+        
+        // 다음 실행되는 dfs()반환값 취합하여 max값 찾기
+        for(int p = 0; p < n; p++){
+            for(int c : tree[p]){
+                if(visited[p] && !visited[c]){
+                    visited[c] = true;
+                    if(info[c] == 0){
+                        ret = dfs(info, tree, visited, sheep + 1, wolf);
+                    }
+                    else{
+                        // wolf + 1??
+                        ret = dfs(info, tree, visited, sheep, wolf+1);
+                    }
+                    visited[c] = false;            
+                    maxSheep = Math.max(maxSheep, ret);
                 }
-                //다음 노드를 방문 표시 해제한다
-                visited[child]=false;
             }
         }
-        return maxsheep;
+        return maxSheep;
     }
+    
 }
