e8f38583896a7c45b5a4f7d00341a271d0d4a3a8
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-10-10 18:54:19 +0900

    [level 3] Title: 광고 삽입, Time: 354.71 ms, Memory: 277 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,55 +1,56 @@
-import java.util.*;
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        String answer = "";
-        int play_time_sec = toSec(play_time);
-        int adv_time_sec = toSec(adv_time);
-        long[] viewers = new long[play_time_sec+1];
-        
-        for(int i=0;i<logs.length;i++){
-            String[] log = logs[i].split("-");
-            int from = toSec(log[0]);
-            int to = toSec(log[1]);
-            viewers[from]+=1;
-            viewers[to]-=1;
+        //✅ 문자열로 주어진 시간을 정수형 단위시간(초)으로 변환하기
+        final int playSec = strToInt(play_time);
+        final int advSec = strToInt(adv_time);
+				//✅ 주어진 로그들을 통해 시작시간과 끝시간에 시청자 수의 변동을 기록한다.
+        long[] delta = new long[playSec+1];
+        for (String log : logs) {
+            String[] split = log.split("-");
+            final int startSec = strToInt(split[0]);
+            final int endSec = strToInt(split[1]);
+						//✅ 시작시간에 +1을 기록한다.
+            delta[startSec]++;
+						//✅ 종료시간에 -1을 기록한다.
+            delta[endSec]--;
         }
-        //시각 별 시청자 수 저장하기
-        for(int i=1;i<viewers.length;i++){
-            viewers[i]+=viewers[i-1];
+				//✅ DP테이블을 순회하며 기록된 정보를 통해 구간별 시청자 수를 기록한다.
+        for (int i = 1; i < delta.length; i++) {
+            delta[i] = delta[i-1] + delta[i];
         }
-        //시각 별 누적 시청 시간 저장하기 viewsum[i] 는 0초부터 i초까지의 누적 시간
-        long[] viewsum = new long[play_time_sec+1];
-        viewsum[0]=viewers[0];
-        for(int i=1;i<viewsum.length;i++){
-            viewsum[i]=viewsum[i-1]+viewers[i];
+				//✅ DP테이블을 순회하며 구간별 시청자 수를 통해 누적 시청자 수를 기록한다.
+        for (int i = 1; i < delta.length; i++) {
+            delta[i] = delta[i-1] + delta[i];
         }
-        //슬라이딩 윈도우
-        long max = 0;
-        int best = 0;
-        for(int start=0;start+adv_time_sec<=play_time_sec;start++){
-            int end = start+adv_time_sec-1;
-            long total = viewsum[end]-(start>0?viewsum[start-1]:0);
-            if(total>max){
-                max = total;
-                best = start;
+				//✅ DP테이블을 순회하며 광고 시간동안 누적 시청자 수가 가장 많은 구간을 구한다.
+        int startTime = 0;
+        long maxTime = delta[advSec-1];
+        for (int i = 0; i < playSec-advSec+1; i++) {
+            if (maxTime < delta[i+advSec] - delta[i]) {
+                maxTime = delta[i+advSec] - delta[i];
+                startTime = i + 1;
             }
         }
-        return toStr(best);
+        return intToStr(startTime);
     }
-    public int toSec(String time){
-        String[]times = time.split(":");
-        int sec = 0;
-        sec += (Integer.parseInt(times[0])*3600);
-        sec += (Integer.parseInt(times[1])*60);
-        sec += Integer.parseInt(times[2]);
-        return sec;
+    
+    int strToInt(String time) {
+        String[] split = time.split(":");
+        int hour = Integer.parseInt(split[0]);
+        int minute = Integer.parseInt(split[1]);
+        int second = Integer.parseInt(split[2]);
+        return 3600*hour + 60*minute + second;
     }
-    public String toStr(int num){
-        int hour = num/3600;
-        num%=3600;
-        int min = num/60;
-        num%=60;
-        int sec = num;
-        return String.format("%02d:%02d:%02d",hour,min,sec);
+    
+    String intToStr(int s) {
+        String sec = String.valueOf(s % 60);
+        if (sec.length() == 1) sec = "0" + sec;
+        s /= 60;
+        String minute = String.valueOf(s % 60);
+        if (minute.length() == 1) minute = "0" + minute;
+        s /= 60;
+        String hour = String.valueOf(s);
+        if (hour.length() == 1) hour = "0" + hour;
+        return hour + ":" + minute + ":" + sec;
     }
 }
